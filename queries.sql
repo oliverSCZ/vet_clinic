e6c5f40e2d608952f9e7375b7394de1581c72f3d
Oliver Coimbra <olivercoimbra@gmail.com>
2022-02-23 07:10:36 -0400

    Fix typo errors

--- queries.sql
+++ queries.sql
@@ -2,9 +2,9 @@
 
 SELECT * FROM animals WHERE name like '%mon';
 SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
-SELECT name	FROM animals WHERE neutered IS true AND escape_attemps < 3;
+SELECT name	FROM animals WHERE neutered IS true AND escape_attempts < 3;
 SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
-SELECT name, escape_attemps	FROM animals WHERE weight_kg > 10.5;
+SELECT name, escape_attempts	FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals	WHERE neutered IS true;
 SELECT * FROM animals	WHERE name NOT LIKE 'Gabumon';
 SELECT * FROM animals	WHERE weight_kg BETWEEN 10.4 AND 17.3
