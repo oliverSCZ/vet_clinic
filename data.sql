9b42baaefa0f025cf06a368fe19a21d54c5c9feb
Oliver Coimbra <olivercoimbra@gmail.com>
2022-02-23 07:10:00 -0400

    Fix typo errors

--- data.sql
+++ data.sql
@@ -1,7 +1,7 @@
 /* Populate database with sample data. */
 
 INSERT INTO public.animals(
-	id, name, date_of_birth, escape_attemps, neutered, weight_kg)
+	id, name, date_of_birth, escape_attempts, neutered, weight_kg)
 	VALUES 
   (1, 'Agumon', '03-02-2020', 0, true, 10.23 );
   (2, 'Gabumon', '15-11-2018', 2, true, 8);
